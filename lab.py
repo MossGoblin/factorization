43047ba4ada1f38adef05212083c6ab8c6b5423b
Angel Slavchev <hugolabs@gmail.com>
2021-05-16 12:29:06 +0300

    Moved number methods outside of the number class

--- lab.py
+++ lab.py
@@ -3,7 +3,6 @@ from typing import List
 
 
 def get_prime_factors(value) -> List:
-    # [...] retork
     prime_factors = []
     primes_below = list(pp.primes_below(value))
     number_body = value
@@ -14,3 +13,33 @@ def get_prime_factors(value) -> List:
             prime_factors.append(prime)
             number_body = number_body / prime
     return prime_factors
+
+
+def get_prime_factors(value) -> List:
+    prime_factors = []
+    primes_below = list(pp.primes_below(int(value / 2)))
+    number_body = value
+    for prime in primes_below:
+        if number_body == 1:
+            break
+        while number_body % prime == 0:
+            prime_factors.append(prime)
+            number_body = number_body / prime
+    return prime_factors
+
+
+def get_prime_mean(prime_factors: List[int]) -> float:
+    prime_sum = 0
+    for prime_factor in prime_factors:
+        prime_sum += prime_factor
+    prime_mean = prime_sum / len(prime_factors)
+    return prime_mean
+
+
+def get_mean_deviation(prime_factors: List[int], prime_mean: float) -> float:
+    deviations_sum = 0
+    for prime_factor in prime_factors:
+        deviations_sum += abs(prime_factor - prime_mean)
+    mean_deviation = deviations_sum / len(prime_factors)
+
+    return mean_deviation
