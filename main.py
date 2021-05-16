9961221dbb19393a1f646719cbc7fe466f8032d0
Angel Slavchev <hugolabs@gmail.com>
2021-05-16 14:26:41 +0300

    Visualization added with bokeh; split first order number

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from configparser import ConfigParser
+from bokeh.plotting import figure, show
+from bokeh import models as models
+from bokeh.models import ColumnDataSource, CategoricalColorMapper
+from typing import List
+import pandas as pd
+
 from number import Number
 
 
@@ -6,23 +12,97 @@ config = ConfigParser()
 
 config.read('config.ini')
 
-lowerbound = int(config.get('setup', 'lowerbound'))
-upperbound = int(config.get('setup', 'upperbound'))
-
-
-# DBG test
-# import lab
-# pf = lab.get_prime_factors(24)
+lowerbound = int(config.get('range', 'lowerbound'))
+upperbound = int(config.get('range', 'upperbound'))
 
 
 def run(lowerbound=2, upperbound=10):
-    # [ ] iterate between bounds and cache numbers
-    number_list = []
-    for value in range(lowerbound, upperbound):
-        number = Number(value=value)
-        number_list.append(number)
+    # [x] iterate between bounds and cache numbers
+    number_list = generate_number_list(
+        lowerbound=lowerbound, upperbound=upperbound)
+
+    # [...] split data
+    running_maximum_deviation = 0
+    for number in number_list:
+        deviation = number.mean_deviation
+        if deviation > running_maximum_deviation:
+            number.first_row = True
+            running_maximum_deviation = deviation
+
+    # [...] craete visualization
+    create_visualization(number_list)
+    pass
+
+
+def create_visualization(number_list: List[Number]):
+    # [ ] prep vis data
+    # prep x-axis and y-axix
+    data_dict = {}    
+    data_dict['number'] = []
+    data_dict['is_prime'] = []
+    data_dict['prime_factors'] = []
+    data_dict['mean'] = []
+    data_dict['deviation'] = []
+    data_dict['first_row'] = []
+    for number in number_list:
+        data_dict['number'].append(number.value)
+        data_dict['is_prime'].append('true' if number.is_prime else 'false')
+        data_dict['prime_factors'].append(number.prime_factors)
+        data_dict['mean'].append(number.prime_mean)
+        data_dict['deviation'].append(number.mean_deviation)
+        data_dict['first_row'].append('true' if number.first_row else 'false')
+
+    data_df = pd.DataFrame(data_dict)
+    data_df.reset_index()
+    data = ColumnDataSource(data=data_df)
+
+    # [x] create plot
+    plot_width = 1600
+    plot_height = 900
+    graph = figure(title=f"Mean prime factor deviations for numbers {data_dict['number'][0]} to {data_dict['number'][-1]}",
+                   x_axis_label='number', y_axis_label='mean prime factor deviation', width=plot_width, height=plot_height)
+
+
+    # [...] add hover tool
+    hover = models.HoverTool(tooltips=[('number', '@number'),
+                                       ('prime', '@is_prime'),
+                                       ('factors', '@prime_factors'),
+                                       ('mean factor value', '@mean'),
+                                       ('mean factor deviation', '@deviation')])
+    graph.add_tools(hover)
+
+    # [] colorize by type
+    prime_color = config.get('graph', 'prime_color')
+    composite_color = config.get('graph', 'composite_color')
+    first_row_color = config.get('graph', 'first_row_color')
+    unmarked_color = config.get('graph', 'unmarked_color')
+    color_mapper = CategoricalColorMapper(factors=['true', 'false'], palette=[first_row_color, unmarked_color]
+    )
+
+    # [x] add graph
+    # graph.scatter(source=data, x='number', y='deviation', color="#386CB0", size=5)
+    graph.scatter(source=data, x='number', y='deviation', color={'field': 'first_row', 'transform': color_mapper}, size=5)
+
+    # [x] show
+    show(graph)
 
     pass
+
+def int_list_to_str(number_list: List[int], separator = ', '):
+    stringified_list = []
+    for number in number_list:
+        stringified_list.append(str(number))
+    
+    return separator.join(stringified_list)
+
+
+def generate_number_list(lowerbound=2, upperbound=10):
+    number_list = []
+    for value in range(lowerbound, upperbound + 1):
+        number = Number(value=value)
+        number_list.append(number)
+    return number_list
+
 
 if __name__ == "__main__":
     run(lowerbound=lowerbound, upperbound=upperbound)
--- number.py
+++ number.py
@@ -6,11 +6,19 @@ class Number():
     def __init__(self, value):
         self.value = value
         self.is_prime = lab.pp.isprime(value)
-        self.prime_factors = []
-        if self.is_prime:
-            self.prime_factors.append(self.value)
+        if self.value == 1:
+            self.prime_mean = 0
+            self.prime_factors = []
+            self.prime_mean = 0
+            self.mean_deviation = 0
         else:
-            self.prime_factors = lab.get_prime_factors(self.value)
-        self.prime_mean = lab.get_prime_mean(self.prime_factors)
-        self.mean_deviation = lab.get_mean_deviation(
-            self.prime_factors, self.prime_mean)
+            self.prime_factors = []
+            if self.is_prime:
+                self.prime_factors.append(self.value)
+            else:
+                self.prime_factors = lab.get_prime_factors(self.value)
+            self.prime_mean = lab.get_prime_mean(self.prime_factors)
+            self.mean_deviation = lab.get_mean_deviation(
+                self.prime_factors, self.prime_mean)
+        
+        self.first_row = False
