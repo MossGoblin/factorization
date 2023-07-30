8d1ab3b272ec2dda1d3f6d99d977d67817b83058
Angel Slavchev <hugolabs@gmail.com>
2023-07-30 14:13:34 +0300

    Align number and lab

--- lab.py
+++ lab.py
@@ -2,34 +2,44 @@ import pyprimes as pp
 from typing import List
 
 
-# def get_prime_factors(value) -> List:
-#     prime_factors = []
-#     primes_below = list(pp.primes_below(value))
-#     number_body = value
-#     for prime in primes_below:
-#         if number_body == 1:
-#             break
-#         while number_body % prime == 0:
-#             prime_factors.append(prime)
-#             number_body = number_body / prime
-#     return prime_factors
+def get_prime_factors(value) -> List:
+    prime_factors = []
+    primes_below = list(pp.primes_below(value))
+    number_body = value
+    for prime in primes_below:
+        if number_body == 1:
+            break
+        while number_body % prime == 0:
+            prime_factors.append(prime)
+            number_body = number_body / prime
+    return prime_factors
 
 
-def get_division_family(factors_list) -> int:
-    division_family = 1
-    for counter in range(len(factors_list) - 1):
-        division_family = division_family * factors_list[counter]
-    return division_family
+def get_prime_factors(value) -> List:
+    prime_factors = []
+    primes_below = list(pp.primes_below(int(value / 2)))
+    number_body = value
+    for prime in primes_below:
+        if number_body == 1:
+            break
+        while number_body % prime == 0:
+            prime_factors.append(prime)
+            number_body = number_body / prime
+    return prime_factors
 
 
-def get_ideal_factor(value: int, prime_factors: List[int]) -> float:
-    return value / len(prime_factors)
+def get_prime_mean(prime_factors: List[int]) -> float:
+    prime_sum = 0
+    for prime_factor in prime_factors:
+        prime_sum += prime_factor
+    prime_mean = prime_sum / len(prime_factors)
+    return prime_mean
 
 
-def get_mean_deviation(prime_factors: List[int], ideal_factor: float) -> float:
+def get_mean_deviation(prime_factors: List[int], prime_mean: float) -> float:
     deviations_sum = 0
     for prime_factor in prime_factors:
-        deviations_sum += abs(prime_factor - ideal_factor)
+        deviations_sum += abs(prime_factor - prime_mean)
     mean_deviation = deviations_sum / len(prime_factors)
 
     return mean_deviation
--- number.py
+++ number.py
@@ -7,22 +7,21 @@ class Number():
         self.value = value
         self.is_prime = lab.pp.isprime(value)
         if self.value == 1:
-            self.ideal_factor = 0
+            self.prime_mean = 0
             self.prime_factors = []
+            self.prime_mean = 0
             self.mean_deviation = 0
-            self.antislope = 0
+            self.slope = 0
         else:
             self.prime_factors = []
             if self.is_prime:
                 self.prime_factors.append(self.value)
-                self.division_family = self.value
             else:
                 self.prime_factors = lab.get_prime_factors(self.value)
-                self.division_family = lab.get_division_family(self.prime_factors)
             self.prime_mean = lab.get_prime_mean(self.prime_factors)
             self.mean_deviation = lab.get_mean_deviation(
-                self.prime_factors, self.ideal_factor)
+                self.prime_factors, self.prime_mean)
             if self.mean_deviation > 0:
-                self.antislope = self.value / self.mean_deviation
+                self.slope = self.value / self.mean_deviation
             else:
-                self.antislope = 0
+                self.slope = 0
